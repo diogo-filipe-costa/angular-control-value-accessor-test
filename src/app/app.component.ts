05b17afe48cbd38e82b9def797c7bf249edec583
Diogo Costa <diogo.filip.costa@gmail.com>
2025-12-19 15:57:03 +0000

    feature: wip before rename

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RatingInputComponent } from "./components/rating-input.component";
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RatingInputComponent, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-app';
+
+  // For template-driven form
+  templateRating = 3;
+
+  // FormControl example
+  ratingControl = new FormControl<number | null>(2);
+
+  // FormGroup example
+  ratingForm = new FormGroup({
+    rating: new FormControl<number | null>(4)
+  });
+
+  // helper methods to demo
+  setAllToFive() {
+    this.templateRating = 5;
+    this.ratingControl.setValue(5);
+    this.ratingForm.get('rating')?.setValue(5);
+  }
+
+  toggleDisableFormControl() {
+    if (this.ratingControl.disabled) this.ratingControl.enable();
+    else this.ratingControl.disable();
+  }
+
+  toggleDisableFormGroup() {
+    const ratingCtrl = this.ratingForm.get('rating');
+    if (!ratingCtrl) return;
+
+    if (ratingCtrl.disabled) ratingCtrl.enable();
+    else ratingCtrl.disable();
+  }
+  
+  patchFormGroupToOne() {
+    this.ratingForm.patchValue({ rating: 1 });
+  }
+
+  resetAll() {
+    this.templateRating = 0;
+    this.ratingControl.reset();
+    this.ratingForm.reset();
+  }
 }
